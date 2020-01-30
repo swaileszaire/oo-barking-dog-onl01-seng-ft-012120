ac3672fef7b9a48ade90f0a6c3258378af3997e3
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-30 03:00:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Dog 
+
+todo = Dog.new
+todo.name = "Todo"
+
+  def name= (this_dogs_name)
+    @this_dogs_name = this_dogs_name
+  end
+
+  
+  def name
+    @this_dogs_name
+  end
+
+  
+  def bark 
+    puts "woof!"
+  end
+end
+
+  
